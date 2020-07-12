1f3219b6929ba4affefc09fd463686b3d8e0f851
Dianna Mack <dianna.mack@yahoo.com>
2020-07-12 14:18:31 -0400

    files

---
+++ script.js
@@ -0,0 +1,34 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+var lowercare ="abcdefghijklmnopqrstuvwxyz",
+  uppercare="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers ="0123456789",
+  puncuation ="!@#$%^&*()_+~|{}[]<>.,-=",
+
+
+var passwordLength === 8;
+
+for (var i=0; i<passwordLength.length; i++)
+  var randomNumber = Math.floor(Math.random())
+
+
+
+// When button is clicked a series of prompts will come up //
+var myName = prompt("What is the length of your password?")
+
+console.log(myName);
+
+
