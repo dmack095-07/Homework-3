a9139cf379bb2380cd492e6d3977ca25c5be8def
Dianna Mack <dianna.mack@yahoo.com>
2020-07-14 23:39:57 -0400

    updated files

--- script.js
+++ script.js
@@ -1,34 +1,74 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+    
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-var lowercare ="abcdefghijklmnopqrstuvwxyz",
-  uppercare="ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  numbers ="0123456789",
-  puncuation ="!@#$%^&*()_+~|{}[]<>.,-=",
 
 
-var passwordLength === 8;
+function generatePassword()  {
+  // assigning code
+  var lowercase ="abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = "!@#$%^&*()_+~|{}[]<>.,-=";
 
-for (var i=0; i<passwordLength.length; i++)
-  var randomNumber = Math.floor(Math.random())
+  lowercaseInput = document.querySelector("lowercase");
+    uppercaseInput = document.getElementById("uppercase");
+    numericInput = document.querySelector("numeric");
+    specialInput = document.getElementById("special");
+  
+  // When button is clicked a series of prompts will come up //
+  var passwordLength = prompt("Choose length of your password?");
+
+  // if user inputs password between 8 and 128.
+  if (passwordLength >= 8 && passwordLength <=128) {
+    console.log(passwordLength)
+  } else {
+    prompt("Your password length must be between 8 and 128 characters.");
+  }
+
+  // if user chooses to have lowercase.//
+  prompt("Would you like lowercase in your password?");
+  if (lowercaseInput) {
+  }
+  // if user chooses to have uppercase.//
+  prompt("Would you like uppercase in your password?");
+  if (uppercaseInput) {
+  } 
+  // if user chooses to have numeric characters.//
+  prompt("Would you like numeric characters in your password?");
+  if (numericInput) {
+  }
+
+  // if user chooses to have special characters.//
+  prompt("Would you like special characters in your password?");
+  if (specialInput) {
+  }
+  
+  //add password to textbox/display area
+  document.getElementsByClassName("display").value = password;
 
 
-
-// When button is clicked a series of prompts will come up //
-var myName = prompt("What is the length of your password?")
-
-console.log(myName);
-
-
+}
+// assign function to all conditions
+function lowercaseInput(){
+      return lowercase[Math.floor(Math.random() * lowercase.length)]
+}
+function lowercaseInput(){
+  return lowercase[Math.floor(Math.random() * lowercase.length)]
+}
+function lowercaseInput(){
+  return lowercase[Math.floor(Math.random() * lowercase.length)]
+}
+function lowercaseInput(){
+  return lowercase[Math.floor(Math.random() * lowercase.length)]
+}
